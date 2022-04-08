324e5be9b5a3fd8e5b0d6a69215117d146a63c67
Javier58525 <javier58525@gmail.com>
2022-04-07 20:28:58 -0500

    Se agrego Login

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # FlASK
 #############################################################
 app = Flask(__name__)
 #############################################################
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/prueba')
-def prueba():
-    return ("a01748965")
+
+@app.route('/signup')
+def signup():
+    name = request.form["name"]
+    email = request.form["email"]
+    password = request.form["password"]
+    return render_template('index.html')
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if (request.method == "GET"):
+        return render_template("login.html", data="email")
+    else:
+        email = None
+        email = request.form["email"]
+        password = request.form["password"]
+        return render_template("index.html", data=email)
